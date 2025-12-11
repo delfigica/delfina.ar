aa4e172a03e73fbad5326b364f33003158ae3635
Delfina Cañas <79326402+delfigica@users.noreply.github.com>
2025-12-11 15:41:54 -0300

    next config output exports

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
-import { i18n } from 'next-i18next';
+import { i18n } from "next-i18next";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-/*   output: "export", */  // <=== enables static exports
+  output: "export", // <=== enables static exports
   reactStrictMode: true,
 };
-
-
 
 export default nextConfig;
